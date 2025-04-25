c4959cea4a28d4269bfdea0f59dcca1bb69aa6c2
EnzoBrancati <enzobrancati28@gmail.com>
2025-04-25 23:11:03 +0200

    Add some meta tags to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,14 @@ export const metadata: Metadata = {
   title: 'Vincent Brancati Portfolio - Web Developer | SEO Specialist',
   description: "Vincent Brancati's web development projects and skills.",
   keywords: ['Vincent Brancati', 'web developer', 'seo specialist'],
+  creator: 'Vincent Brancati',
+  publisher: 'Vincent Brancati',
   robots: {
     index: true,
     follow: true,
+  },
+  alternates: {
+    canonical: 'https://www.vincentbrancati.com',
   },
   openGraph: {
     title: 'Vincent Brancati Portfolio - Web Developer | SEO Specialist',
@@ -15,6 +20,7 @@ export const metadata: Metadata = {
     siteName: 'Vincent Brancati Portfolio',
     locale: 'en_US',
     type: 'website',
+    url: 'https://www.vincentbrancati.com/',
   }
 };
 
@@ -30,6 +36,24 @@ export default function RootLayout({
           rel="stylesheet"
           type="text/css"
           href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org/',
+              '@type': 'Person',
+              name: 'Vincent Brancati',
+              url: 'https://www.vincentbrancati.com/',
+              jobTitle: 'Web Developer',
+              description: 'Full Stack Developer & SEO specialist',
+              sameAs: [
+                'https://github.com/ebrancati',
+                'https://www.linkedin.com/in/enzo-brancati-a2880520b/',
+                'https://www.youtube.com/@e.brancati'
+              ]
+            })
+          }}
         />
       </head>
       <body>{children}</body>
